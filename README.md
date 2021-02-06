9940123a565c738c24505fd1c5af9d422bd774c0
Jonathan Chow <jonathan.chow@mclaren.com>
2021-02-06 18:42:55 +0000

    Basic CRUD operation

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # python-postgressql-poke-around
+
+Folloing instruction on this [link](https://stackabuse.com/working-with-postgresql-in-python/)
+
+Useful SQL syntax and youtube [link](https://www.youtube.com/watch?v=5tEApCGgpEQ&ab_channel=BecomingaDataScientist) 
+ - SELECT * FROM student
+ - UPDATE STUDENT set AGE = 20 where ADMISSION = 3420 
+ - DELETE from STUDENT where ADMISSION=3420
--- connect_test.py
+++ connect_test.py
@@ -22,3 +22,48 @@ user, db, password, host, port = get_credentials()
 con = psycopg2.connect(database="postgres", user="postgres", password=password, host="127.0.0.1", port="5432")
 
 print("Database opened successfully")
+
+cur = con.cursor()
+# cur.execute('''CREATE TABLE STUDENT
+#       (ADMISSION INT PRIMARY KEY     NOT NULL,
+#       NAME           TEXT    NOT NULL,
+#       AGE            INT     NOT NULL,
+#       COURSE        CHAR(50),
+#       DEPARTMENT        CHAR(50));''')
+# print("Table created successfully")
+
+# cur.execute("INSERT INTO STUDENT (ADMISSION,NAME,AGE,COURSE,DEPARTMENT) VALUES (3420, 'John', 18, 'Computer Science', 'ICT')");
+# cur.execute("INSERT INTO STUDENT (ADMISSION,NAME,AGE,COURSE,DEPARTMENT) VALUES (3419, 'Abel', 17, 'Computer Science', 'ICT')");
+# cur.execute("INSERT INTO STUDENT (ADMISSION,NAME,AGE,COURSE,DEPARTMENT) VALUES (3421, 'Joel', 17, 'Computer Science', 'ICT')");
+# cur.execute("INSERT INTO STUDENT (ADMISSION,NAME,AGE,COURSE,DEPARTMENT) VALUES (3422, 'Antony', 19, 'Electrical Engineering', 'Engineering')");
+# cur.execute("INSERT INTO STUDENT (ADMISSION,NAME,AGE,COURSE,DEPARTMENT) VALUES (3423, 'Alice', 18, 'Information Technology', 'ICT')");
+# print("Added student")
+
+cur.execute("SELECT admission, name, age, course, department from STUDENT")
+rows = cur.fetchall()
+
+for row in rows:
+    print("ADMISSION =", row[0])
+    print("NAME =", row[1])
+    print("AGE =", row[2])
+    print("COURSE =", row[3])
+    print("DEPARTMENT =", row[4], "\n")
+
+cur.execute("UPDATE STUDENT set AGE = 20 where ADMISSION = 3419")
+con.commit()
+
+cur.execute("DELETE from STUDENT where ADMISSION=3420;")
+con.commit()
+
+cur.execute("SELECT admission, name, age, course, department from STUDENT")
+rows = cur.fetchall()
+
+for row in rows:
+    print("ADMISSION =", row[0])
+    print("NAME =", row[1])
+    print("AGE =", row[2])
+    print("COURSE =", row[3])
+    print("DEPARTMENT =", row[4], "\n")
+
+con.commit()
+con.close()
